1d956d647cb3fee910dc2c7a29c36baabbdadbac
Thomas Lei <thomas_lei@tutk.com>
2015-09-19 23:23:01 +0800

    Align licence

--- printrun/burner.py
+++ printrun/burner.py
@@ -805,18 +805,14 @@ class PronterWindow(MainWindow, pronsole.pronsole):
                           '(C)  Layer One Co., Ltd.')
         info.SetWebSite('www.atom3dp.com')
 
-        licence = """\
-Burner is free software: you can redistribute it and/or modify it under the
-terms of the GNU General Public License as published by the Free Software
-Foundation, either version 3 of the License, or (at your option) any later
-version.
-
-Burner is distributed in the hope that it will be useful, but WITHOUT ANY
-WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
-PARTICULAR PURPOSE.  See the GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License along with
-Printrun. If not, see <http://www.gnu.org/licenses/>."""
+        licence = "Burner is free software: you can redistribute it and/or modify it under the "
+        licence += " terms of the GNU General Public License as published by the Free Software Foundation, either"
+        licence += " version 3 of the License, or (at your option) any later version.\n"
+        licence += "Burner is distributed in the hope that it will be useful, but WITHOUT ANY "
+        licence += "WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A "
+        licence += "PARTICULAR PURPOSE.  See the GNU General Public License for more details. \n"
+        licence += "You should have received a copy of the GNU General Public License along with"
+        licence += "Printrun. If not, see <http://www.gnu.org/licenses/>."
 
         info.SetLicence(licence)
         info.AddDeveloper('Coby')
